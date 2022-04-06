89707636de1c403a52693e100d16f7d161bc42bd
madhav2k21 <madhav.anupoju.2k21@gmail.com>
2022-04-06 22:12:49 +0530

    integration tests

--- src/test/java/com/techleads/app/integration/UserControllerITests.java
+++ src/test/java/com/techleads/app/integration/UserControllerITests.java
@@ -2,12 +2,16 @@ package com.techleads.app.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -92,9 +96,10 @@ public class UserControllerITests {
 		
 		user = Users.builder()
 				.name("madhav").location("Hyderabad").build();
-		userRepository.save(user);
+		Users save = userRepository.save(user);
+		Integer id=save.getId();
 		// when -behavior that we are going to test or action
-		ResultActions response = mockMvc.perform(get("/users/" + 107));
+		ResultActions response = mockMvc.perform(get("/users/{id}",id));
 		// then -verify the output
 		response.andExpect(status().isOk()).andDo(print()).andExpect(jsonPath("$.name", is(user.getName())))
 				.andExpect(jsonPath("$.location", is(user.getLocation())))
@@ -113,6 +118,80 @@ public class UserControllerITests {
 		ResultActions response = mockMvc.perform(get("/users/" + id));
 		// then -verify the output
 		response.andExpect(status().isNotFound()).andDo(print());
+	}
+	
+	
+	@Test
+	@DisplayName("deleteUserById")
+	public void givenUserId_whenDeleteById_thenReturnResult() throws Exception {
+		// given -pre condition or setup
+		
+		user = Users.builder()
+				.name("madhav").location("Hyderabad").build();
+		userRepository.save(user);
+		Integer id = 107;
+		// when -behavior that we are going to test
+		ResultActions perform = mockMvc.perform(delete("/users/{id}", id));
+		//then
+		MvcResult andReturn = perform.andReturn();
+		System.out.println(andReturn.getResponse().getContentAsString());
+		assertThat(andReturn.getResponse().getContentAsString()).isEqualTo("User is deleted  successfully with Id: " + id);
+		perform.andExpect(status().isOk())
+		.andDo(print());
+	
+
+	}
+	
+	@Test
+	@DisplayName("deleteUserById_Negative")
+	public void givenUserId_whenDeleteById_thenReturnErrorResult() throws Exception {
+		// given -pre condition or setup
+		Integer id = 1001;
+		// when -behavior that we are going to test
+		ResultActions perform = mockMvc.perform(delete("/users/{id}", id));
+		//then
+		MvcResult andReturn = perform.andReturn();
+		System.out.println(andReturn.getResponse().getContentAsString());
+		assertThat(andReturn.getResponse().getContentAsString()).isEqualTo("Failed to Delete the user");
+		perform.andExpect(status().isOk())
+		.andDo(print());
+
+	}
+	
+	@Test
+	@DisplayName("updateUserById_fixed")
+	public void givenIdAndUserObj_whenUpdateUserById_thenReturnResult() throws JsonProcessingException, Exception {
+		// given -pre condition or setup
+		user = Users.builder()
+				.name("madhav").location("Hyderabad").build();
+		Users saved = userRepository.save(user);
+		
+		Users updatedUser = Users.builder()
+				.name("madhav").location("Hyderabad").build();
+		// when -behavior that we are going to test
+		ResultActions response = mockMvc.perform(put("/users/{id}", saved.getId()).contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(updatedUser)));
+		response.andDo(print()).andExpect(status().isOk());
+		MvcResult andReturn = response.andReturn();
+		System.out.println("{}:::"+andReturn.getResponse().getContentAsString());
+	}
+	
+	@Test
+	@DisplayName("updateUserById_fixed_Negative")
+	public void givenIdAndUserObj_whenUpdateUserById_thenReturnErrorMessage() throws JsonProcessingException, Exception {
+		// given -pre condition or setup
+		// given -pre condition or setup
+				user = Users.builder()
+						.name("madhav").location("Hyderabad").build();
+				Users saved = userRepository.save(user);
+		Integer id = 1001;
+
+		// when -behavior that we are going to test
+		ResultActions response = mockMvc.perform(put("/users/{id}", id).contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(user)));
+		response.andDo(print()).andExpect(status().isOk());
+		MvcResult andReturn = response.andReturn();
+		System.out.println("{}:::"+andReturn.getResponse().getContentAsString());
 	}
 
 }
